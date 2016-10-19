5100aaedb2251bce864d3c6bebe3f1be173cdac8
Dane Sirois <dane_sirois@live.com>
2016-10-19 04:20:38 +0000

    save

--- database.js
+++ database.js
@@ -16,6 +16,22 @@ let database = {
   }
 };
 
+
+let database = {
+  urls: {
+    "b2xVn2": {url: "www.lighthouselabs.ca", userId: "3aeu4u9"} ,
+    "9sm5xK": {url: "www.google.com", userId: "3aeu4u9"}
+  },
+  users: {
+    "3aeu4u9": { 
+      id: '3aeu4u9',
+      username: 'Cooldude49',
+      email: 'cooldude49@live.com',
+      password: 'password'
+    }
+  }
+};
+
 module.exports = {
   database
 }; 
